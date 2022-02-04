2ca7f5b77c2426eb39bdd2c481b07bd5733a5b06
clrosenbluth <clrosenbluth@gmail.com>
2022-02-04 13:33:19 -0500

    Added QuadraticEquationMain class

--- src/main/java/ProjectileMotion.java
+++ src/main/java/ProjectileMotion.java
@@ -4,11 +4,7 @@ public class ProjectileMotion
     {
         Projectile p = new Projectile(52, 43);
 
-//        System.out.println("x = " + p.getX(7));
-//        System.out.println("y = " + p.getY(7));
-
-        QuadraticEquation q = new QuadraticEquation(1, 5, 6);
-        double[] xVals = q.getX();
-        System.out.println("x values: " + xVals[0] + ", " + xVals[1]);
+        System.out.println("x = " + p.getX(7));
+        System.out.println("y = " + p.getY(7));
     }
 }
